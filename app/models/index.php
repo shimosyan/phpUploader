608ec08d6fccc60500f374ebcdf210b10c9c74d0
shimosyan <owner@cube-unit.net>
2025-08-02 01:51:52 +0900

    refactor: クラス名とメソッドの構造を改善
    - `index` クラスを `Index` にリネームし、PSR-1に準拠
    - メソッドの可読性を向上させるためにインデントを修正
    - 名前空間の宣言を追加し、依存関係を明示化

--- app/models/index.php
+++ app/models/index.php
@@ -1,36 +1,43 @@
 <?php
-class index {
-  function index() {
 
-    $config = new config();
-    $ret = $config->index();
-    //配列キーが設定されている配列なら展開
-    if (!is_null($ret)) {
-      if(is_array($ret)){
-        extract($ret);
-      }
+namespace phpUploader\Models;
+
+use PDO;
+use Exception;
+
+class Index
+{
+    public function index()
+    {
+        $config = new config();
+        $ret = $config->index();
+        //配列キーが設定されている配列なら展開
+        if (!is_null($ret)) {
+            if (is_array($ret)) {
+                extract($ret);
+            }
+        }
+
+        //データベースの作成・オープン
+        try {
+            $db = new PDO('sqlite:' . $db_directory . '/uploader.db');
+        } catch (Exception $e) {
+            $error = '500 - データベースの接続に失敗しました。';
+            include('./app/views/header.php');
+            include('./app/views/error.php');
+            include('./app/views/footer.php');
+            exit;
+        }
+
+        // デフォルトのフェッチモードを連想配列形式に設定
+        // (毎回PDO::FETCH_ASSOCを指定する必要が無くなる)
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        // 選択 (プリペアドステートメント)
+        $stmt = $db->prepare("SELECT * FROM uploaded");
+        $stmt->execute();
+        $r = $stmt->fetchAll();
+
+        return array('data' => $r);
     }
-
-    //データベースの作成・オープン
-    try{
-      $db = new PDO('sqlite:'.$db_directory.'/uploader.db');
-    }catch (Exception $e){
-      $error = '500 - データベースの接続に失敗しました。';
-      include('./app/views/header.php');
-      include('./app/views/error.php');
-      include('./app/views/footer.php');
-      exit;
-    }
-
-    // デフォルトのフェッチモードを連想配列形式に設定
-    // (毎回PDO::FETCH_ASSOCを指定する必要が無くなる)
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    // 選択 (プリペアドステートメント)
-    $stmt = $db->prepare("SELECT * FROM uploaded");
-    $stmt->execute();
-    $r = $stmt->fetchAll();
-
-    return array('data' => $r);
-  }
 }
