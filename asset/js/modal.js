117b32a9e0cff942542fa5553c34755546c0f98e
shimosyan <owner@cube-unit.net>
2025-08-02 01:52:50 +0900

    Refactor FileManager and Modal JavaScript classes for improved readability and consistency
    - Updated the FileManager class to use consistent indentation and formatting for methods.
    - Added new methods for external access: refresh, search, sort, and goToPage.
    - Enhanced the applyFilters method to improve search and sorting functionality.
    - Refactored modal.js to standardize function formatting and improve readability.

--- asset/js/modal.js
+++ asset/js/modal.js
@@ -1,32 +1,31 @@
 
-function openModal(type, title, body, action){
-  switch (type){
-    case 'okcansel':
-      $('#OKCanselModal .modal-title').html(title);
-      $('#OKCanselModal .modal-body').html(body);
+function openModal(type, title, body, action)
+{
+    switch (type) {
+        case 'okcansel':
+            $('#OKCanselModal .modal-title').html(title);
+            $('#OKCanselModal .modal-body').html(body);
 
-      if(action != null){
-        $('#OKCanselModal .f-action').attr('onclick', action);
-      }
-      $('#OKCanselModal').modal('show');
+            if (action != null) {
+                $('#OKCanselModal .f-action').attr('onclick', action);
+            }
+            $('#OKModal').modal('show');
 
-      break;
-    case 'ok':
-      $('#OKModal .modal-title').html(title);
-      $('#OKModal .modal-body').html(body);
+            break;
+        case 'ok':
+            $('#OKModal .modal-title').html(title);
+            $('#OKModal .modal-body').html(body);
 
-      if(action != null){
-        $('#OKModal .f-action').attr('onclick', action);
-
-      }
-      $('#OKModal').modal('show');
-      break;
-
-  }
-
+            if (action != null) {
+                $('#OKModal .f-action').attr('onclick', action);
+            }
+            $('#OKModal').modal('show');
+            break;
+    }
 }
 
-function closeModal(){
-  $('#OKCanselModal').modal('hide');
-  $('#OKModal').modal('hide');
+function closeModal()
+{
+    $('#OKCanselModal').modal('hide');
+    $('#OKModal').modal('hide');
 }
