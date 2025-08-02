6247b4faa2457d71b0875b32752c8656a0440ed7
shimosyan <owner@cube-unit.net>
2025-08-02 18:54:34 +0900

    fix: Lint Error

--- app/models/index.php
+++ app/models/index.php
@@ -2,35 +2,38 @@
 
 namespace PHPUploader\Model;
 
-class Index {
-  function index() {
+class Index
+{
+    public function index()
+    {
+        $config = new \PHPUploader\Config();
+        $ret = $config->index();
+        //配列キーが設定されている配列なら展開
+        if (!is_null($ret)) {
+            if (is_array($ret)) {
+                extract($ret);
+            }
+        }
 
-    $config = new \PHPUploader\Config();
-    $ret = $config->index();
-    //配列キーが設定されている配列なら展開
-    if (!is_null($ret)) {
-      if(is_array($ret)){
-        extract($ret);
-      }
+        //データベースの作成・オープン
+        try {
+            $db = new \PDO('sqlite:' . $ret['db_directory'] . '/uploader.db');
+        } catch (Exception $e) {
+            $error = '500 - データベースの接続に失敗しました。';
+            exit;
+        }
+
+        // デフォルトのフェッチモードを連想配列形式に設定
+        // (毎回\PDO::FETCH_ASSOCを指定する必要が無くなる)
+        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
+        // 選択 (プリペアドステートメント)
+        $stmt = $db->prepare('SELECT * FROM uploaded');
+        $stmt->execute();
+        $r = $stmt->fetchAll();
+
+        return [
+            'data' => $r,
+        ];
     }
-
-    //データベースの作成・オープン
-    try{
-      $db = new \PDO('sqlite:'.$db_directory.'/uploader.db');
-    }catch (Exception $e){
-      $error = '500 - データベースの接続に失敗しました。';
-      exit;
-    }
-
-    // デフォルトのフェッチモードを連想配列形式に設定
-    // (毎回\PDO::FETCH_ASSOCを指定する必要が無くなる)
-    $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-
-    // 選択 (プリペアドステートメント)
-    $stmt = $db->prepare("SELECT * FROM uploaded");
-    $stmt->execute();
-    $r = $stmt->fetchAll();
-
-    return array('data' => $r);
-  }
 }
